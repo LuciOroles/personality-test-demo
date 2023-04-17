4d605810a490302237a91f6a7acdce11dddfc597
LuciOroles <stoica.florin.lucian@gmail.com>
2023-04-17 16:16:49 +0300

    minimal webhosting

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export default {
+    PORT: 8000,
+    build: 'build',
+    exceptEndpoints: ['/get-data']
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const greeting= 'Hello!';
-console.log(greeting);
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import staticFiles from './staticFiles';
+
+import constatns from './constants';
+
+const { PORT, build, exceptEndpoints } = constatns;
+const app = express();
+
+app.use(express.static(path.resolve(`./${build}`)));
+
+app.get("*", staticFiles(exceptEndpoints));
+app.get('/get-data', (req: Request, res: Response) => {
+    const data = { message: 'This is some data from the server!' };
+    res.json(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`App live :${PORT}`);
+});
