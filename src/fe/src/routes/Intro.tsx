410bb1dc9617f1fb443a16caa6214b9c20d3bb17
LuciOroles <stoica.florin.lucian@gmail.com>
2023-04-20 13:54:47 +0300

    env logic

--- src/fe/src/routes/Intro.tsx
+++ src/fe/src/routes/Intro.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import {  useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Box, Button, Heading, Image } from "@chakra-ui/react";
 
 import Page from "../UI/Page";
